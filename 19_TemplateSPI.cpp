3a1f3978a2c6aafa7600798609bc649bbbbcd131
victorenrique-gilaltamirano <115146273+victorenrique-gilaltamirano@users.noreply.github.com>
2022-11-17 13:16:45 +0700

    Create 19_TemplateSPI.cpp

---
+++ 19_TemplateSPI.cpp
@@ -0,0 +1,24 @@
+#include "mbed.h"
+
+SPI spi(PinSPI_COTI, PinSPIT_CITO, PinSPI_SCLK);
+
+DigitalOut cs(PinSPI_CS);
+
+int main()
+{
+  cs = 1; //start with target DESELECTED
+  
+  //setup SPI for 8 bit, high steady state clock
+  //second edge capture, and 1MHz clock rate
+  spi.format(8,3);
+  spi.frequency(1000000);
+  
+  cs = 0; // SELECT the target
+  
+  spi.write(0x8F); //send 0x8F device address data
+  
+  //store the answer of the Target, in register whoami 0x00
+  printf("WHOAMI regsiter  = 0x%X\n", whoami);
+  
+  cs = 1; //DESELECT device
+}
